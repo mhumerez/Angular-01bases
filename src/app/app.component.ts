1f4bf3ffa2c2faeb21e57f87efa365fb7e35aafd
Marcelo Humerez <marcelohumerez@gmail.com>
2021-11-26 09:28:27 -0500

    primer commit proyecto angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+  //template: '<span>Marcelo</span>'
+})
+export class AppComponent {
+  public titulo:string = 'Contador App';
+}
