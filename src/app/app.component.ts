2ee17306e8118c5e327f6ff0b5b9b38141341bc0
Marcelo Humerez Mars <mhumerez@tecnimotors.com>
2021-11-27 15:41:46 -0500

    Se crea modulo componente Contador.component: app-contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,7 @@ import { Component } from '@angular/core';
   // se trabaja html en esta misma hoja
 })
 export class AppComponent {
-  public titulo:string = 'Contador -- App';
-  numero: number = 10;
-
-  //metodo 3
-
-  base  :number = 5
-
-
-//sumar y restart METODO 1
- // sumar() {
- //   this.numero += 1;
- // }
-
- // restar() {
- //   this.numero -= 1;
- // }
-
-  //sumar y restar METODO 2
-
-//  acumular(valor: number) {
-//    this.numero+= valor;
-//  }
-
-
-//sumar y restar BASE 5 tarea METODO 3
-
-    acumular(valor: number) {
-      this.numero += valor;
-    }
+  
+  
 
 }
--- src/app/contador.component.ts
+++ src/app/contador.component.ts
@@ -13,44 +13,19 @@ import { Component } from "@angular/core";
     <span> {{numero}} </span>
 
     <button (click)="acumular(-base)"> - 1 </button>
-
-
-
-
-    
     
     `
 }) 
 
 export class ContadorComponent {
 
-    public titulo:string = 'Contador -- App';
+  titulo:string = 'Contador -- App';
   numero: number = 10;
-
-  //metodo 3
-
   base  :number = 5
 
-
-//sumar y restart METODO 1
- // sumar() {
- //   this.numero += 1;
- // }
-
- // restar() {
- //   this.numero -= 1;
- // }
-
-  //sumar y restar METODO 2
-
-//  acumular(valor: number) {
-//    this.numero+= valor;
-//  }
-
-
-//sumar y restar BASE 5 tarea METODO 3
-
-    acumular(valor: number) {
+  acumular(valor: number) {
       this.numero += valor;
+
+}
 
 }
