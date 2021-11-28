23c5a6e218de9caae82c06123b4d35a2c1cdbd06
Marcelo Humerez RBS <mhumerez@tecnimotors.com>
2021-11-28 01:43:44 -0500

    Listado de Heroes. Primer metodo

--- src/app/heroes/listado/listado.component.ts
+++ src/app/heroes/listado/listado.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
 })
-export class ListadoComponent implements OnInit {
+export class ListadoComponent {
 
-  constructor() { 
-    console.log('constructor');
-  }
-
-  ngOnInit(): void {
-    console.log('ngOnInit');
-  }
+  heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
 
 }
